0e6c7c97acbd6d06da2d604dce513c8ce78194e2
SoraLuiGheoan <andreea_anghelescu20@yahoo.com>
2016-10-28 21:58:00 +0300

    adauga diferenta

--- suma a 2 numere.cpp
+++ suma a 2 numere.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 using namespace std;
 int main ()
-{ int a,b,c,d;
+{ int a,b,c,d,e;
   cin>>a>>b;
   c=a+b;
   d=c/2;
+  e=a-b;
   cout<<c<<endl;
+  cout<<e<<endl;
   cout<<d;
   return 0;
 } 
