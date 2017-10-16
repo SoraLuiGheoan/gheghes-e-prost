3c0c251eba14348d47f2fca70cde7d3aef4cc077
Andea <andreea_anghelescu20@yahoo.com>
2017-10-16 20:14:22 +0300

    Create prefixe.cpp

---
+++ prefixe.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main ()
+{
+    char a[12];
+    int i,j;
+    cin>>a;
+    for(i=strlen(b)-1;i>0;i--)
+        {for(j=0;j<i;j++)
+            cout<<b[j];
+        cout<<endl;
+        }
+    return 0;
+}
